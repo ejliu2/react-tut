c2e0993e4c439912a96eac1d06c2bd81130209ad
Eric L <ejl2@sfu.ca>
2021-07-03 16:13:24 -0700

    finished widget project

--- widgets/src/App.js
+++ widgets/src/App.js
@@ -1,6 +1,11 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Accordion from './components/Accordion';
-
+import Search from './components/Search';
+import Dropdown from './components/Dropdown';
+import Translate from './components/Translate';
+import Route from './components/Route';
+import Header from './components/Header';
+import Link from './components/Link';
 const items = [
     {
         title: 'What is React?',
@@ -16,10 +21,71 @@ const items = [
     }
 ];
 
+const options = [
+    {
+        label: 'The Color Red',
+        value: 'red'
+    },
+    {
+        label: 'The Color Green',
+        value: 'green'
+    },
+    {
+        label: 'The Color Blue',
+        value: 'blue'
+    }
+];
+
+const showAccordion = () => {
+    if (window.location.pathname === '/') {
+        return (
+            <Accordion items={items} />
+        );
+    };
+};
+
+const showList = () => {
+    if (window.location.pathname === '/list') {
+        return (
+            <Search />
+        );
+    };
+};
+
+const showDropdown = () => {
+    if (window.location.pathname === '/dropdown') {
+        return (
+            <Dropdown />
+        );
+    };
+};
+
+const showTranslate = () => {
+    if (window.location.pathname === '/translate') {
+        return (
+            <Translate />
+        );
+    };
+};
+
 export default () => {
-	return (
+
+    const [selected, setSelected] = useState(options[0]);
+    return (
         <div>
-            <Accordion items={items}/>
+            <Header />
+            <Route path="/">
+                <Accordion items={items} />
+            </Route>
+            <Route path="/list">
+                <Search />
+            </Route>
+            <Route path="/dropdown">
+                <Dropdown label="Select a color" options={options} selected={selected} onSelectedChange={setSelected} />
+            </Route>
+            <Route path="/translate">
+                <Translate />
+            </Route>
         </div>
-	);
+    );
 };
